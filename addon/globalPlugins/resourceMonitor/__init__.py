caf57a29d7bab70a32ded12f8b5ff78e928b5e17
Mesar Hameed <mhameed@src.gnome.org>
2013-03-03 11:45:22 +0100

    attempt to improve the translatable messages.

--- addon/globalPlugins/resourceMonitor/__init__.py
+++ addon/globalPlugins/resourceMonitor/__init__.py
@@ -31,67 +31,79 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
  def script_announceBatteryInfo(self, gesture):
   info=""
-  battery.getInfo() #returns nothing, but sets vars we can now inspect
-  if battery.noBattery: info=_("This computer does not have a battery connected.")
-  elif not battery.onBattery: info=_("%s%%, battery is charging.") %(tryTrunk(battery.percentage))
-  elif battery.onBattery: #discharging battery, so provide info on it
-   info=_("%s%% (about %s) battery remaining. ") %(tryTrunk(battery.percentage), battery.timeLeft)
+  #returns nothing, but sets vars we can now inspect
+  battery.getInfo()
+  if battery.noBattery:
+   info=_("This computer does not have a battery connected.")
+  elif not battery.onBattery: 
+   # Translators: message presented when AC is connected and battery is charging, also show current battery percentage.
+   info=_("{percent}%%, charging battery.").format(percent=tryTrunk(battery.percentage))
+  elif battery.onBattery: 
+   # Translators: message presented when computer running on battery power, showing percentage remaining and estimated remaining time.
+   info=_("{percent}%% battery remaining, about {time}.").format(percent=tryTrunk(battery.percentage), time=battery.timeLeft)
    if battery.low:
-    info+=_(" Battery is low")
+    info+=_("Warning: low battery.")
    elif battery.critical:
-    info+=_(" Battery is critical")
+    info+=_("Warning: battery is critically low.")
   ui.message(info)
- script_announceBatteryInfo.__doc__=_("Speaks battery percentage, charging status, time left (if not charging), and a warning if the battery is low or critical.")
+ script_announceBatteryInfo.__doc__=_("Presents battery percentage, charging status, remaining time (if not charging), and a warning if the battery is low or critical.")
 
  def script_announceDriveInfo(self, gesture):
   #goes through all registered drives and gives info on each one
   info=""
-  for drive in psutil.disk_partitions(): #get all registered drives
+  #get all registered drives
+  for drive in psutil.disk_partitions():
    try:
-    driveInfo=psutil.disk_usage(drive[0]) #get info on each one
-    #Translators: Shows drive letter, type of drive (fixed or removable), used capacity and total capacity of a drive (example: C, fixed drive; 40 GB of 100 GB used (40%). Note that %s cannot be changed.
-    info+=_("%s (%s drive): %s of %s used (%s%%). ") %(drive[0], drive[2], toBiggestBytes(tryTrunk(driveInfo[1])), toBiggestBytes(tryTrunk(driveInfo[0])), tryTrunk(driveInfo[3]))
-   except: pass
+    #get info on each one
+    driveInfo=psutil.disk_usage(drive[0])
+    # Translators: Shows drive letter, type of drive (fixed or removable), used capacity and total capacity of a drive (example: C drive, ntfs; 40 GB of 100 GB used (40%).
+    info+=_("{driveName} ({driveType} drive): {usedSpace} of {totalSpace} used {percent}%%.").format(driveName=drive[0], driveType=drive[2], usedSpace=toBiggestBytes(tryTrunk(driveInfo[1])), totalSpace=toBiggestBytes(tryTrunk(driveInfo[0])), percent=tryTrunk(driveInfo[3]))
+   except:
+    pass
   ui.message(info)
- script_announceDriveInfo.__doc__=_("Speaks the used and total space of every drive registered on this computer, along with each drive's type (fixed, removeable, or CDROM).")
+ script_announceDriveInfo.__doc__=_("Presents the used and total space of the static and removable drives on this computer.")
 
  def script_announceProcessorInfo(self, gesture):
   cores=psutil.NUM_CPUS #number of cores
   averageLoad=psutil.cpu_percent()
-  perCpuLoad=psutil.cpu_percent(percpu=True) #lists load for each core
+  #lists load for each core
+  perCpuLoad=psutil.cpu_percent(percpu=True)
   coreLoad=""
   # Translators: Shows average load of CPU cores (example: core 1, 50%).
-  for i in range(len(perCpuLoad)): coreLoad+="Core %s: %s%%. " %(str(i+1), tryTrunk(perCpuLoad[i]))
-  #Translators: Shows average load of the processor (example: 15% average load). The second %s denotes core number.
+  for i in range(len(perCpuLoad)):
+   coreLoad+="Core %s: %s%%. " %(str(i+1), tryTrunk(perCpuLoad[i]))
+  # Translators: Shows average load of the processor (example: 15% average load). The second %s 
+denotes core number.
   info=_("%s%% average CPU load. %s") %(tryTrunk(averageLoad), coreLoad)
   ui.message(info)
- script_announceProcessorInfo.__doc__=_("Speaks the average processor load, then the load of each core.")
+ script_announceProcessorInfo.__doc__=_("presents the average processor load, then the load of each core.")
 
  def script_announceRamInfo(self, gesture):
   ram=psutil.phymem_usage()
   # Translators: Shows RAM (physical memory) usage.
-  info=_("Physical: %s of %s used (%s%%). ") %(toBiggestBytes(tryTrunk(ram[1])), toBiggestBytes(tryTrunk(ram[0])), tryTrunk(ram[3]))
+  info=_("Physical: %s of %s used (%s%%).") %(toBiggestBytes(tryTrunk(ram[1])), toBiggestBytes(tryTrunk(ram[0])), tryTrunk(ram[3]))
   virtualRam=psutil.virtmem_usage()
   # Translators: Shows virtual memory usage (first %s is used space, second %s is total capacity, third %s is usage in percentage).
   info+=_("Virtual: %s of %s used (%s%%).") %(toBiggestBytes(tryTrunk(virtualRam[1])), toBiggestBytes(tryTrunk(virtualRam[0])), tryTrunk(virtualRam[3]))
   ui.message(info)
- script_announceRamInfo.__doc__=_("Speaks the used and total space for both physical and virtual ram.")
+ script_announceRamInfo.__doc__=_("Presents the used and total space for both physical and virtual ram.")
 
  def script_announceResourceSummary(self, gesture):
   cpuLoad=psutil.cpu_percent()
   ram=psutil.phymem_usage()
   freeRam=ram[3]
-  # Translators: shows the overal summary of resource usage, such as CPU load and RAM usage.
+  # Translators: presents the overall summary of resource usage, such as CPU load and RAM usage.
   info=(_("%s%% ram used, CPU at %s%%.") %(tryTrunk(freeRam), tryTrunk(cpuLoad)))
   battery.getInfo()
   if not battery.noBattery and not battery.batteryStatusUnknown and not battery.onBatteryUnknown:
-   if not battery.onBattery: info+=_(" Battery is charging, at %s%%.") %(tryTrunk(battery.percentage))
-   elif battery.onBattery: #discharging battery, so provide info on it
-    info+=_(" %s%% (about %s) battery remaining. ") %(tryTrunk(battery.percentage), battery.timeLeft)
-    if battery.low: info+=_(" Battery is low")
-    elif battery.critical: info+=_(" Battery is critical")
+   if not battery.onBattery: info+=_("Battery %s%%, charging.") %(tryTrunk(battery.percentage))
+   elif battery.onBattery:
+    #discharging battery, so provide info on it
+    info+=_("%s%% battery remaining, about %s.") %(tryTrunk(battery.percentage), battery.timeLeft)
+    if battery.low: info+=_(" Warning: low battery.")
+    elif battery.critical: info+=_(" Warning: critically low battery.")
   ui.message(info)
- script_announceResourceSummary.__doc__=_("Speaks used ram, average processor load, and battery info (if this computer has a battery).")
+ script_announceResourceSummary.__doc__=_("Presents used ram, average processor load, and battery info if available.")
 
  __gestures={
   "KB:NVDA+shift+e":"announceResourceSummary",
