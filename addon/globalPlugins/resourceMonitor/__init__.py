9c71324a29b1a8191ae8c9821ff2db4c8949dd22
Joseph Lee <joseph.lee22590@gmail.com>
2014-10-03 11:32:11 -0700

    Wndows 10 support: obtain the build number for the tech preview - to be removed later.

--- addon/globalPlugins/resourceMonitor/__init__.py
+++ addon/globalPlugins/resourceMonitor/__init__.py
@@ -154,14 +154,23 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			if winMinor == 0: winverName = "Windows Vista" if server == 1 else "Windows Server 2008" # Vista.
 			elif winMinor == 1: winverName = "Windows 7" if server == 1 else "Windows Server 2008 R2" # Windows 7
 			elif winMinor == 2: winverName = "Windows 8" if server == 1 else "Windows Server 2012" # Windows 8.
-			elif winMinor == 3: winverName = "Windows 8.1" if server == 1 else "Windows Server 2012 R2" # Windows 8.1.
-			elif winMinor == 4: winverName = "Windows 10" if server == 1 else "Windows Server 10" # Windows Threshold.
+			elif winMinor == 3:
+				import _winreg
+				# Python issue 19143: in Windows 8.1, sys module reports NT 6.2 when in fact the version is 6.3.
+				# A suggestion posted there (quoting a Stack Overflow post) is to ask the registry to find out the real version value.
+				win10testKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Microsoft\Windows NT\CurrentVersion")
+				winValue, type = _winreg.QueryValueEx(win10testKey, "CurrentVersion")
+				buildNum, type2 = _winreg.QueryValueEx(win10testKey, "BuildNumber")
+				_winreg.CloseKey(win10testKey)
+				if winValue == "6.4": winverName = "Windows 10 Tech Preview" if server == 1 else "Windows Server 10" # Windows Threshold.
+				else:  winverName = "Windows 8.1" if server == 1 else "Windows Server 2012 R2" # Windows 8.1.
 		# Translators: Presented under 64-bit Windows.
 		if is64Bit: x64 = _("64-bit")
 		# Translators: Presented under 32-bit Windows.
 		else: x64 = _("32-bit")
 		# Translators: Presents Windows version (example output: "Windows version: Windows XP (32-bit)").
 		if not sp: info = _("Windows version: {winVersion} ({cpuBit})").format(winVersion = winverName, cpuBit = x64)
+		if winValue == "6.4": info = info + " build {build}".format(build = buildNum)
 		# Translators: Presents Windows version and service pack level (example output: "Windows version: Windows 7 service pack 1 (64-bit)").
 		else: info = _("Windows version: {winVersion} {servicePackLevel} ({cpuBit})").format(winVersion = winverName, servicePackLevel = sp, cpuBit = x64)
 		return info
