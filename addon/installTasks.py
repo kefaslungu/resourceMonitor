efef59c36c41c81b165c37acc138b0ceeee807f4
Joseph Lee <joseph.lee22590@gmail.com>
2018-02-09 09:48:29 -0800

    Requires Windows 7 or Server 2008 R2 SP1 or later

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -12,9 +12,9 @@ import addonHandler
 addonHandler.initTranslation()
 
 def onInstall():
-	requiredVer = "Windows Vista Service Pack 2, Windows Server 2003 Service Pack 1"
+	requiredVer = "Windows 7 Service Pack 1, Windows Server 2008 R2 Service Pack 1"
 	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
-	if sys.getwindowsversion().build < 3790 and gui.messageBox(_("Support for Windows XP from Resource Monitor add-on is ending on December 2017. Future add-on releases will require {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
+	if sys.getwindowsversion().build < 7601 and gui.messageBox(_("Support for Windows Vista, Server 2003 and 2008 from Resource Monitor add-on is ending on June 2018. Future add-on releases will require {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
 	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
 		raise RuntimeError("Old Windows version detected")
