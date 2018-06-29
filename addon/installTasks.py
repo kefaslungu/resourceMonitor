c513e1ab065dce59fffb169e63ad83b5be9c5d64
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-29 09:30:43 -0700

    Readme and installer: completely drop support for Windows releases older than 7 SP1.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -14,7 +14,7 @@ addonHandler.initTranslation()
 def onInstall():
 	requiredVer = "Windows 7 Service Pack 1, Windows Server 2008 R2 Service Pack 1"
 	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
-	if sys.getwindowsversion().build < 7601 and gui.messageBox(_("Support for Windows Vista, Server 2003 and 2008 from Resource Monitor add-on is ending on June 2018. Future add-on releases will require {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
+	if sys.getwindowsversion().build < 7601 and gui.messageBox(_("You are using an old version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
 	# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 	_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION) == wx.NO:
 		raise RuntimeError("Old Windows version detected")
--- readme.md
+++ readme.md
@@ -23,7 +23,7 @@ This add-on does not replace task manager and other system information programs 
 
 * CPU usage is given for logical processors, not physical cores. This is noticeable for processors which uses Hyper-Threading where number of CPU's is twice the number of CPU cores.
 * If there is heavy disk activity such as copying large files, there might be delays when obtaining disk usage information.
-* Support for Windows XP from this add-on ended on December 31, 2017. Support for Windows Server 2003, Vista and Server 2008 will end on June 30, 2018.
+* Support for Windows XP from this add-on ended on December 31, 2017. Support for Windows Server 2003, Vista and Server 2008 ended on June 30, 2018.
 
 ## Version 18.04
 
