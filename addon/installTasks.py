3364ffe4089a0a7b612c5cfbd7c88a20265ef76a
Joseph Lee <joseph.lee22590@gmail.com>
2020-04-27 14:15:18 -0700

    Install tasks: import modules when running onInstall.
    After speech refactor in a beta snapshot in April 2020, NVDA will emit a critical error message after installing the add-on. This was traced to both NVDA (imports) and the add-on install tasks (importing modules early). To get around this, import needed modules when running onInstall instead of at the top.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -5,13 +5,13 @@
 # Mostly checks compatibility.
 # Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
 
-import sys
-import gui
-import wx
 import addonHandler
 addonHandler.initTranslation()
 
 def onInstall():
+	import sys
+	import gui
+	import wx
 	requiredVer = "Windows 7 Service Pack 1, Windows Server 2008 R2 Service Pack 1"
 	# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
 	if sys.getwindowsversion().build < 7601 and gui.messageBox(_("You are using an old version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion = requiredVer),
