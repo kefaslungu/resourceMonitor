b76edf21c2b861f2e0ed67e02c994acd6b08489e
Joseph Lee <joseph.lee22590@gmail.com>
2020-06-29 20:22:06 -0700

    Resource Monitor 20.07
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("A handy resource monitor to report CPU load, memory usage, battery, disk usage status and more."),
 	# version
-	"addon_version": "20.06",
+	"addon_version": "20.07",
 	# Author(s)
 	"addon_author": "Alex Hall <mehgcap@gmail.com>, Joseph Lee <joseph.lee22590@gmail.com>, beqa gozalishvili <beqaprogger@gmail.com>, Tuukka Ojala <tuukka.ojala@gmail.com>, Ethin Probst <harlydavidsen@gmail.com> and other NVDA contributors",
 	# URL for the add-on documentation support
