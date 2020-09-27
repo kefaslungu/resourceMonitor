2c359918ec6a21168ef0febf09dbb50d85fa71db
Joseph Lee <joseph.lee22590@gmail.com>
2020-09-27 09:51:53 -0700

    Install tasks: split lengthy source code and translator comments.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -3,7 +3,8 @@
 
 # Provides needed routines during add-on installation and removal.
 # Mostly checks compatibility.
-# Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
+# Routines are partly based on other add-ons,
+# particularly Place Markers by Noelia Martinez (thanks add-on authors).
 
 import addonHandler
 addonHandler.initTranslation()
@@ -15,7 +16,8 @@ def onInstall():
 	import wx
 	requiredVer = "Windows 7 Service Pack 1, Windows Server 2008 R2 Service Pack 1"
 	if sys.getwindowsversion().build < 7601 and gui.messageBox(
-		# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows (minSupportedVersion is the minimum version required for this add-on).
+		# Translators: Dialog text shown when attempting to install the add-on on an unsupported version of Windows
+		# (minSupportedVersion is the minimum version required for this add-on).
 		_("You are using an old version of Windows. This add-on requires {minSupportedVersion} or later. Are you sure you wish to install this add-on anyway?").format(minSupportedVersion=requiredVer),
 		# Translators: title of the dialog shown when attempting to install the add-on on an old version of Windows.
 		_("Old Windows version"), wx.YES | wx.NO | wx.CANCEL | wx.CENTER | wx.ICON_QUESTION
