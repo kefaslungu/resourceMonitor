09bd3a38d7a29a166ed06ab808c25c69d623593c
Joseph Lee <joseph.lee22590@gmail.com>
2022-03-13 13:07:48 -0700

    Instal tasks (Flake8 E501): remove words from comments

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -3,7 +3,7 @@
 
 # Provides needed routines during add-on installation and removal.
 # Mostly checks compatibility.
-# Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
+# Partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
 
 import addonHandler
 addonHandler.initTranslation()
