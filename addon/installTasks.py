58c67de51a86d1779c8d7e15c98bcdd7a0429c10
Joseph Lee <joseph.lee22590@gmail.com>
2022-03-17 10:44:27 -0700

    Install tasks: Windows 10 or later is required

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -16,10 +16,8 @@ def onInstall():
 	if winVersion.getWinVer() < winVersion.WIN10:
 		gui.messageBox(
 			_(
-				# Translators: Dialog text shown when attempting to install the add-on on an older version of Windows.
-				"You are using an older version of Windows. "
-				"This is the last version of Resource Monitor add-on to support this version of Windows. "
-				"Future add-on releases will require Windows 10 or later."
+				# Translators: Dialog text shown when trying to install the add-on on an older version of Windows.
+				"You are using an older version of Windows. This add-on requires Windows 10 or later."
 			),
 			# Translators: title of the dialog shown when trying to install the add-on on an old version of Windows.
 			_("Old Windows version"), wx.OK | wx.ICON_WARNING
