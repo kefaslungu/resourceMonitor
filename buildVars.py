72a57d0e030ae473b14cb8accd7e646feb984907
Joseph Lee <joseph.lee22590@gmail.com>
2022-11-26 01:31:21 -0800

    Add-on template: use 2022.3 edition (add-on store JSON generator added)

--- buildVars.py
+++ buildVars.py
@@ -30,6 +30,8 @@ addon_info = {
 	"addon_author": "Alex Hall <mehgcap@gmail.com>, Joseph Lee <joseph.lee22590@gmail.com>, beqa gozalishvili <beqaprogger@gmail.com>, Tuukka Ojala <tuukka.ojala@gmail.com>, Ethin Probst <harlydavidsen@gmail.com> and other NVDA contributors",
 	# URL for the add-on documentation support
 	"addon_url": "https://addons.nvda-project.org/",
+	# URL for the add-on repository where the source code can be found
+	"addon_sourceURL": "https://github.com/josephsl/resourcemonitor",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
@@ -40,6 +42,10 @@ addon_info = {
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
+	# Add-on license such as GPL 2
+	"addon_license": "GPL v2",
+	# URL for the license document the ad-on is licensed under
+	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
 }
 
 
