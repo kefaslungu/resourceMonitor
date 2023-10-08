8177ce517fb23bb889d5723cdbfef1b647aa4089
Joseph Lee <joseph.lee22590@gmail.com>
2023-10-08 09:39:10 -0600

    Flake8 E501: shorten long lines seen in wi-fi message handler/queue

--- addon/globalPlugins/resourceMonitor/__init__.py
+++ addon/globalPlugins/resourceMonitor/__init__.py
@@ -55,14 +55,26 @@ try:
 			return
 		if pData.contents.NotificationCode == wlanapi.wlan_notification_acm_connection_complete:
 			ssid = wlanapi.WLAN_CONNECTION_NOTIFICATION_DATA.from_address(pData.contents.pData).dot11Ssid.SSID
-			queueHandler.queueFunction(queueHandler.eventQueue, message, _("Connected to {}").format(ssid.decode("utf-8")), "connect.wav")
+			queueHandler.queueFunction(
+				queueHandler.eventQueue, message,
+				_("Connected to {}").format(ssid.decode("utf-8")), "connect.wav"
+			)
 		elif pData.contents.NotificationCode == wlanapi.wlan_notification_acm_disconnected:
 			ssid = wlanapi.WLAN_CONNECTION_NOTIFICATION_DATA.from_address(pData.contents.pData).dot11Ssid.SSID
-			queueHandler.queueFunction(queueHandler.eventQueue, message, _("Disconnected from {}").format(ssid.decode("utf-8")), "disconnect.wav")
+			queueHandler.queueFunction(
+				queueHandler.eventQueue, message,
+				_("Disconnected from {}").format(ssid.decode("utf-8")), "disconnect.wav"
+			)
 		elif pData.contents.NotificationCode == wlanapi.wlan_notification_acm_interface_arrival:
-			queueHandler.queueFunction(queueHandler.eventQueue, message, _("A wireless device has been enabled"), "connect.wav")
+			queueHandler.queueFunction(
+				queueHandler.eventQueue, message,
+				_("A wireless device has been enabled"), "connect.wav"
+			)
 		elif pData.contents.NotificationCode == wlanapi.wlan_notification_acm_interface_removal:
-			queueHandler.queueFunction(queueHandler.eventQueue, message, _("A wireless device has been disabled"), "disconnect.wav")
+			queueHandler.queueFunction(
+				queueHandler.eventQueue, message,
+				_("A wireless device has been disabled"), "disconnect.wav"
+			)
 except NameError:
 	pass
 
