5ece1b6d8450a392dc7a14c75108c3c72e507855
Danstiv <50794055+Danstiv@users.noreply.github.com>
2025-01-12 22:38:44 +0700

    Change spaces to tabs in the memory module

--- addon/globalPlugins/resourceMonitor/memory.py
+++ addon/globalPlugins/resourceMonitor/memory.py
@@ -2,10 +2,10 @@ from . import psutil
 
 
 def get_physical_memory():
-    memory = psutil.virtual_memory()
-    return memory.used, memory.total
+	memory = psutil.virtual_memory()
+	return memory.used, memory.total
 
 
 def get_virtual_memory():
-    memory = psutil._psutil_windows.virtual_mem()
-    return memory[2]-memory[3], memory[2]
+	memory = psutil._psutil_windows.virtual_mem()
+	return memory[2]-memory[3], memory[2]
