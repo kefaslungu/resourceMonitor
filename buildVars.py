9b967e8dfaa5cd39b11e2407c51e19dc76b791c9
Joseph Lee <joseph.lee22590@gmail.com>
2026-05-22 19:50:06 -0600

    Resource Monitor 26.06
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("A handy resource monitor to report CPU load, memory usage, battery, disk usage status and more."),
 	# version
-	addon_version="26.05",
+	addon_version="26.06",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""* Added an unassigned command to announce GPU (graphics processing unit) information (contributed by kevinderome)."""),
