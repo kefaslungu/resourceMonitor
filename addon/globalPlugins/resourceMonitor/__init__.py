0014fa0188cc8a14e73017ddb9944080e45fb949
Joseph Lee <joseph.lee22590@gmail.com>
2026-09-18 23:59:23 -0600

    Global plugin: remove NVDA 2013.3 qualifier part of script category translator comment

--- addon/globalPlugins/resourceMonitor/__init__.py
+++ addon/globalPlugins/resourceMonitor/__init__.py
@@ -341,7 +341,7 @@ def getWinVer() -> str:
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	# Translators: The gestures category for this add-on in input gestures dialog (2013.3 or later).
+	# Translators: The gestures category for this add-on in input gestures dialog.
 	scriptCategory = _("Resource Monitor")
 	_layerEntryGesture = "kb:NVDA+shift+e"
 	__layerGestures: ClassVar = {
